0302548576d683b07a7bc11507cd9daac052e35f
Simon-Muchemi <simonkenya846@gmail.com>
2023-06-16 11:44:20 +0300

    JavaScript Code --deploy1

--- script.js
+++ script.js
@@ -1,87 +1,20 @@
+const day_input = document.getElementById('day');
+const month_input = document.getElementById('month');
+const year_input = document.getElementById('year');
+
 const button = document.getElementById('calc-button');
-const dayLabel = document.getElementById('day-label');
-const monthLabel = document.getElementById('month-label');
-const yearLabel = document.getElementById('year-label');
 
-const day = document.getElementById('day');
-const month = document.getElementById('month');
-const year = document.getElementById('year');
+const day_input_error = document.getElementById('day-error');
+const month_input_error = document.getElementById('month-error');
+const year_input_error = document.getElementById('year-error');
 
-const dayError = document.getElementById('day-error');
-const invalidDayError = document.getElementById('day-invalid-error')
+let calc_day = document.getElementById('calc-day');
 
-const monthError = document.getElementById('month-error');
-const invalidMonthError = document.getElementById('month-invalid-error');
+let date_today = new Date();
+let current_year = date_today.getFullYear();
+let current_month = date_today.getMonth() + 1;
+let current_day = date_today.getDate();
 
-const yearError = document.getElementById('year-error');
-const invalidYearError = document.getElementById('year-invalid-error');
-const pastYearError = document.getElementById('past-year-error');
-
-const calcDay = document.getElementById('calc-day');
-const calcMonth = document.getElementById('calc-month');
-const calcYear = document.getElementById('calc-year');
-
-const month_with_31_days = [1, 3, 5, 7, 8, 10, 12];
-
-let has_31_days = function (month){
-    if (month === 1){
-        console.log(month);
-        console.log('Month has 31 days');
-        return true;
-    }
-    else {
-        console.log('Oops!')
-        return false;
-    }
-}
-function computeAge() {
-    let today = new Date();
-
-    let Day = today.getDay();
-    let Month = today.getMonth();
-    let Year = today.getFullYear();
-    
-    let usrDay = day.value;
-    let usrMonth = month.value;
-    let usrYear = year.value;
-    
-    let sum = 0;
-    let years, months, days;
-
-    let checkYear = function (){
-        if (usrYear < 1) {
-            yearLabel.style.color = 'hsl(0, 100%, 67%)';
-            invalidYearError.style.display = 'grid';
-        }
-        if (usrYear > Year) {
-            yearLabel.style.color = 'hsl(0, 100%, 67%)';
-            pastYearError.style.display = 'grid';
-        }
-        if (usrYear === undefined) {
-            yearLabel.style.color = 'hsl(0, 100%, 67%)';
-            yearError.style.display = 'grid';
-        }
-    }
-    let checkMonth = function (){
-        if (usrMonth < 1 || usrMonth > 12) {
-            monthLabel.style.color = 'hsl(0, 100%, 67%)';
-            invalidMonthError.style.display = 'grid';
-            return false;
-        }
-        if (usrMonth === undefined) {
-            monthLabel.style.color = 'hsl(0, 100%, 67%)';
-            monthError.style.display = 'grid';
-            return false;
-        }
-        return true;
-    }
-    let checkDay = function (){
-        let index;
-        if (checkMonth()){
-        }
-    }
-    //checkDay();
-    has_31_days(usrMonth);
-}
-
-button.addEventListener('click', computeAge);
+button.addEventListener('click', function (){
+    calc_day.textContent = '24';
+})
